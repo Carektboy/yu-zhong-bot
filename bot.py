d8b69b57455eb71c029d9669df006ceed29ab0f0
Carektboy <99059870+Carektboy@users.noreply.github.com>
2025-05-31 23:59:15 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,19 @@
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Bot is alive"
+
+def run_flask():
+    app.run(host='0.0.0.0', port=10000)
+
+
+threading.Thread(target=run_flask).start()
+
+
 from bardapi import Bard
 import discord
 import os
